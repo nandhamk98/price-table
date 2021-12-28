5af1fed736ba3e6e3bb168f43db742ca97f635ff
Nandhakumar <nandhakumarmk98@gmail.com>
2021-12-28 14:41:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const data = [
+    {
+      subscription: "FREE",
+      price: "$0",
+      duration: "month",
+      benifits: [
+        "Single User",
+        "5GB Storage",
+        "Unlimited Public Projects",
+        "Community Access",
+      ],
+      notAvailable: [
+        "Unlimited Private Projects",
+        "Dedicated Phone Support",
+        "Free Subdomain",
+        "Monthly Status Reports",
+      ],
+    },
+    {
+      subscription: "PLUS",
+      price: "$9",
+      duration: "month",
+      benifits: [
+        "S5 Users",
+        "50GB Storage",
+        "Unlimited Public Projects",
+        "Community Access",
+        "Unlimited Private Projects",
+        "Dedicated Phone Support",
+        "Free Subdomain",
+      ],
+      notAvailable: ["Monthly Status Reports"],
+    },
+    {
+      subscription: "PRO",
+      price: "$9",
+      duration: "month",
+      benifits: [
+        "S5 Users",
+        "50GB Storage",
+        "Unlimited Public Projects",
+        "Community Access",
+        "Unlimited Private Projects",
+        "Dedicated Phone Support",
+        "Free Subdomain",
+        "Monthly Status Reports",
+      ],
+      notAvailable: [],
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Plan</h1>
+      <div className="planList">
+        {data.map((contents) => (
+          <PlanCard
+            subscription={contents.subscription}
+            project={contents.price}
+            duration={contents.duration}
+            benifits={contents.benifits}
+            price={contents.price}
+            notAvailable={contents.notAvailable}
+          />
+        ))}
+        {/* <PlanCard /> */}
+      </div>
+    </div>
+  );
+}
+
+function PlanCard({ subscription, price, duration, benifits, notAvailable }) {
+  return (
+    <div className="planCard">
+      <h2 className="planTitle">{subscription}</h2>
+      <p>
+        <span className="price">{price}</span>
+        <sub>/{duration}</sub>
+      </p>
+      <hr />
+      <BenifitsList benifits={benifits} symbol="✔️" />
+      <BenifitsList benifits={notAvailable} symbol="❌" />
+      <input type="button" value="BUTTON" className="button" />
+    </div>
+  );
+}
+
+function BenifitsList({ benifits, symbol }) {
+  return (
+    <div className="planBenifits">
+      <ul>
+        {benifits.map((benefit) => (
+          <li>
+            <span className="symbol">{symbol}</span> {benefit}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
